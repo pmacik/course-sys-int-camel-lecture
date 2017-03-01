cc3e7e0e8f7998e4a32825e2d4ab1eaf686332bf
Pavel Macík <pmacik@redhat.com>
2017-03-01 23:21:56 +0100

    Updated

--- 01-bean-and-procesor/maven-camel-bean/src/main/java/com/redhat/brq/integration/MyRouteBuilder.java
+++ 01-bean-and-procesor/maven-camel-bean/src/main/java/com/redhat/brq/integration/MyRouteBuilder.java
@@ -14,7 +14,7 @@ public class MyRouteBuilder extends RouteBuilder {
 
       // here is a sample which processes the input files
       // (leaving them in place - see the 'noop' flag)
-      // then performs content based routing on the message using XPath
+      // then processes the messages by MyBean's process method
          from("file:src/data?noop=true")
                .bean(new MyBean(), "process");
    }
